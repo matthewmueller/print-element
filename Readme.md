8f7ed1a52ecd502c955369b45f82cfdd0bf5f081
Matt Mueller <mattmuelle@gmail.com>
2013-07-04 18:23:32 -0700

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,26 @@
+
+# print-element
+
+  simply print out element name, class name, id if present
+
+## Installation
+
+    $ component install matthewmueller/print-element
+
+## API
+
+### print(el)
+
+Print out an element.
+
+```html
+<ul id="list" class="fancy many cool">
+```
+
+```js
+  print(ul) // ul#list.fancy.many.cool
+```
+
+## License
+
+  MIT
---
+++ index.js
@@ -0,0 +1,24 @@
+/**
+ * Expose `print`
+ */
+
+module.exports = print;
+
+/**
+ * Initialize `print`
+ *
+ * @param {Element} print
+ * @return {String}
+ */
+
+function print(el) {
+  if (1 != el.nodeType) throw Error('Expecting an element node');
+  var tagName = el.tagName.toLowerCase();
+  var className = el.className.replace(/\s+/g, '.');
+  var id = el.id;
+
+  if (className) className = '.' + className;
+  if (id) id = '#' + id;
+
+  return tagName + id + className;
+}
